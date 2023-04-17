21906087d8544cae05e5b56c1677ae7977a4e85b
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-17 22:32:01 +0430

    Change Layout => src/layout/Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,16 +7,11 @@ type TLayoutProps = {
 
 const Layout : React.FunctionComponent<TLayoutProps> = ({children}) : JSX.Element => {
      return (  
-          <main className='w-full flex flex-col items-center justify-center pb-28'>
+          <main className='w-full flex flex-col items-center justify-center'>
                <Header/>
 
-               <main className='h-auto w-full grid grid-cols-1 lg:grid-cols-9 gap-x-6 max-w-[1600px]'>
-                    {/* <Sidebar/> */}
-
-                    {/* Main Content */}
-                    <main className='px-6 col-span-7 h-auto'>
-                         {children}
-                    </main>
+               <main className='w-full h-auto'>
+                    {children}
                </main>
           </main>
      );
