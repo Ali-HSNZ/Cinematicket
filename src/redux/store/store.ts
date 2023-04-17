1d38e64fbb492f8484c5388fd7ebfa1ec7c7b83c
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-17 13:04:29 +0430

    use 'next-redux-wrapper' in store redux

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,10 +1,20 @@
-import { configureStore  } from "@reduxjs/toolkit";
+import { ThunkAction, configureStore ,Action} from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import {createWrapper} from 'next-redux-wrapper'
+// import {Action} from 'redux'
 
-
-export const store = configureStore({
-     reducer : rootReducer
+const store = configureStore({
+  reducer : rootReducer,
+  devTools : true,
 })
 
-export type TRootState = ReturnType<typeof store.getState>
+const makeStore = () => store
+
+export type TRootState = ReturnType<AppStore['getState']>
 export type TAppDispatch = typeof store.dispatch
+
+export type AppStore = ReturnType<typeof makeStore>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,TRootState, unknown, Action>
+
+export const wrapper = createWrapper<AppStore>(makeStore)
+
