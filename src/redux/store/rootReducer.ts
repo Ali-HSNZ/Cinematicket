ad9ddf38402fa9b0fa2cd3feec1ffb99da6653e0
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-17 13:03:42 +0430

    Change rootReducer

--- src/redux/store/rootReducer.ts
+++ src/redux/store/rootReducer.ts
@@ -1,7 +1,12 @@
-import {combineReducers} from '@reduxjs/toolkit'
+import { combineReducers } from '@reduxjs/toolkit';
+
+import { authLoginReducer } from '@slices/auth/login'
+import { authRegisterReducer } from '@slices/auth/register'
 
 const rootReducer = combineReducers({
-     
+     // auth
+     authRegister : authRegisterReducer,
+     authLogin : authLoginReducer,
 })
-
+export type RootState = ReturnType<typeof rootReducer>;
 export default rootReducer
