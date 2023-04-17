fa85587dbd13397e1cf11da20513e472e60394b9
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-17 13:32:30 +0430

    Use 'next-redux-wrapper' And Create 'loadUserInfo' For Login User After Refreshed Page in _app => src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,38 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
-import { store } from '@store/store'
-import { Provider } from 'react-redux'
+import { TAppDispatch, wrapper} from '@store/store'
+// Toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// More
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { http , apikey } from '@services/http';
+import { userToken } from '@utils/cookie';
 
-export default function App({ Component, pageProps }: AppProps) {
+
+function App({ Component, pageProps }: AppProps) {
+
+  const dispatch = useDispatch<TAppDispatch>()
+  
+  useEffect(()=>{
+    const loadUserInfo = async() => {
+      const data = await http.get('/user/profile', {headers : {apikey , token : userToken}})
+       .then(res => res.data)
+       .catch(error => {
+            const message = error?.response?.data?.errors ?? "متاسفانه ورود شما با خطا مواجه شده است."
+            throw {message}
+       })
+       return data
+    }
+  },[])
+
   return (
-    <Provider store={store}>
+    <>
       <Component {...pageProps} />
-    </Provider>
+      <ToastContainer position="top-right" autoClose={4000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={true} pauseOnFocusLoss draggable pauseOnHover theme="dark" /> 
+    </>
+
   )
 }
+export default wrapper.withRedux(App);
