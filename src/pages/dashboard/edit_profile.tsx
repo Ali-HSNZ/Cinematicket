3b0f11770aae45154a8281e8302671343699e5a5
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-19 14:05:43 +0430

    import/use 'formik' for validating input - edit profile in user dashboard

--- src/pages/dashboard/edit_profile.tsx
+++ src/pages/dashboard/edit_profile.tsx
@@ -1,21 +1,46 @@
 import Layout from "@layout/Layout"
 import Dashboard_Layout from "@layout/dashboard/Dashboard_Layout";
+import { VALIDATION_EMAIL, VALIDATION_PHONE_NUMBER } from "@utils/valiation";
 import { useFormik } from "formik";
+import * as Yup from 'yup'
+import DatePicker,{DateObject} from "react-multi-date-picker"
+import type{Value} from "react-multi-date-picker"
+import Icon from "react-multi-date-picker/components/icon"
+import persian from "react-date-object/calendars/persian"
+import persian_fa from "react-date-object/locales/persian_fa"
+import "react-multi-date-picker/styles/layouts/mobile.css"
+import {useState} from 'react'
 
 const EditProfile = () => {
 
      const onSubmit = () => {
 
      }
+     const [birthday, setBirthday] = useState<Value>()
+
+     const weekDays = ["ش", "ی", "د", "س", "چ", "پ", "ج"]
+
+     const validationSchema = Yup.object({
+          full_name : Yup.string()
+               .min(5 , "نام و نام خانوادگی نمی تواند کم‌تر از 5 نویسه باشذ")
+               .trim()
+               .max(50,"نام و نام خانوادگی نمی تواند بیشتر از 50 نویسه باشد"),
+          tel : Yup.string()
+               .trim()
+               .matches(VALIDATION_PHONE_NUMBER , "شماره موبایل معتبر نمی‌باشد."),
+          email : Yup.string()
+                  .matches(VALIDATION_EMAIL , "ایمیل معتبر نمی‌باشد.")
+     }) 
 
      const formik = useFormik({
           initialValues : {
                full_name : "",
                tel : "",
                email : '',
-
+               birthday : '',
           },
-          onSubmit
+          onSubmit,
+          validationSchema
      })
 
      return (  
@@ -38,29 +63,52 @@ const EditProfile = () => {
                                    <svg className="w-5 h-5 absolute top-11 right-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                    </svg>
-
-                                   <input maxLength={24} type="tel" name="password" onChange={formik.handleChange} onBlur={formik.handleBlur} className={`${formik.errors.password && formik.touched.password ? "bg-red-50 border-red-200 focus:border-red-300 hover:border-red-300" : "bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300"} mt-2  placeholder:text-gray-500 text-gray-600  py-4 pl-2 pr-9 rounded-md font-iranyekan-regular text-sm border   focus:outline-none w-full`} />
+                                   <input maxLength={50} placeholder="اصغر فرهادی" type="tel" name="full_name" onChange={formik.handleChange} onBlur={formik.handleBlur} className={`${formik.errors.full_name && formik.touched.full_name ? "bg-red-50 border-red-200 focus:border-red-300 hover:border-red-300" : "bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300"} mt-2  placeholder:text-gray-400 text-gray-600  py-4 pl-2 pr-9 rounded-md placeholder:text-sm  font-iranyekan-regular text-sm border   focus:outline-none w-full`} />
+                                   {formik.errors.full_name && formik.touched.full_name && <p className="font-iranyekan-regular text-xs mt-2 text-red-600">{formik.errors.full_name}</p>}
                               </div>
                               <div className="w-full relative">
                                    <p className="font-iranyekan-regular text-gray-500 text-sm">شماره موبایل</p>
                                    <svg className="w-5 h-5 absolute top-11 right-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
                                    </svg>
-                                   <input maxLength={24} type="tel" name="password" onChange={formik.handleChange} onBlur={formik.handleBlur} className={`${formik.errors.password && formik.touched.password ? "bg-red-50 border-red-200 focus:border-red-300 hover:border-red-300" : "bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300"} mt-2  placeholder:text-gray-500 text-gray-600  py-4 pl-2 pr-9 rounded-md font-iranyekan-regular text-sm border   focus:outline-none w-full`} />
+                                   <input maxLength={11} placeholder="09123456789" type="tel" name="tel" onChange={formik.handleChange} onBlur={formik.handleBlur} className={`${formik.errors.tel && formik.touched.tel ? "bg-red-50 border-red-200 focus:border-red-300 hover:border-red-300" : "bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300"} mt-2  placeholder:text-gray-400 text-gray-600  py-4 pl-2 pr-9 rounded-md placeholder:text-sm  font-iranyekan-regular text-sm border   focus:outline-none w-full`} />
+                                   {formik.errors.tel && formik.touched.tel && <p className="font-iranyekan-regular text-xs mt-2 text-red-600">{formik.errors.tel}</p>}
                               </div>
                               <div className="w-full relative">
                                    <p className="font-iranyekan-regular text-gray-500 text-sm">ایمیل</p>
-                                   <svg className="w-5 h-5 absolute top-11 right-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                   <svg className="w-5 h-5 absolute top-11 left-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                         <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
                                    </svg>
-                                   <input maxLength={24} type="tel" name="password" onChange={formik.handleChange} onBlur={formik.handleBlur} className={`${formik.errors.password && formik.touched.password ? "bg-red-50 border-red-200 focus:border-red-300 hover:border-red-300" : "bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300"} mt-2  placeholder:text-gray-500 text-gray-600  py-4 pl-2 pr-9 rounded-md font-iranyekan-regular text-sm border   focus:outline-none w-full`} />
+                                   <input maxLength={60} placeholder="cinematicket@email.com" type="email" dir="ltr" name="email" onChange={formik.handleChange} onBlur={formik.handleBlur} className={`${formik.errors.email && formik.touched.email ? "bg-red-50 border-red-200 focus:border-red-300 hover:border-red-300" : "bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300"} mt-2  placeholder:text-gray-400 text-gray-600  py-4 pr-2 pl-9 rounded-md placeholder:text-sm  font-iranyekan-regular text-sm border   focus:outline-none w-full`} />
+                                   {formik.errors.email && formik.touched.email && <p className="font-iranyekan-regular text-xs mt-2 text-red-600">{formik.errors.email}</p>}
                               </div>
-                              <div className="w-full relative">
-                                   <p className="font-iranyekan-regular text-gray-500 text-sm">تاریخ</p>
+                              <div className="w-full h-fit relative">
+                                   <p className="absolute top-0 w-full font-iranyekan-regular text-gray-500 text-sm">تاریخ</p>
                                    <svg className="w-5 h-5 absolute top-11 right-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                                    </svg>
-                                   <input maxLength={24} type="tel" name="password" onChange={formik.handleChange} onBlur={formik.handleBlur} className={`${formik.errors.password && formik.touched.password ? "bg-red-50 border-red-200 focus:border-red-300 hover:border-red-300" : "bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300"} mt-2  placeholder:text-gray-500 text-gray-600  py-4 pl-2 pr-9 rounded-md font-iranyekan-regular text-sm border   focus:outline-none w-full`} />
+                                   <DatePicker
+                                        value={birthday}
+                                        onChange={setBirthday}
+                                        buttons={true}
+                                        render={(value, openCalendar) => {
+                                             return (
+                                               <button onClick={openCalendar} className="absolute top-9 left-2 p-2 bg-white rounded-full">
+                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-red-500">
+                                                       <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                                                  </svg>
+                                               </button>
+                                             )
+                                        }}     
+                                        calendar={persian}
+                                        weekDays={weekDays}
+                                        locale={persian_fa}
+                                        inputClass=""
+                                        className="rmdp-mobile font-iranyekan-regular"
+                                   />
+                                   <p className={`mt-1.5 bg-gray-50 border-gray-200 hover:border-gray-300 focus:border-gray-300 placeholder:text-gray-400 text-gray-600  py-4 pl-2 pr-9 rounded-md placeholder:text-sm  font-iranyekan-regular text-sm border   focus:outline-none w-full`}>
+                                        {birthday ? birthday?.toLocaleString('fa-IR') : "yyyy/mm/dd"}
+                                   </p>
                               </div>
                          </div>
                          <hr className="mt-6"/>
