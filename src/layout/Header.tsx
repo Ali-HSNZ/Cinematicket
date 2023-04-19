fe5d2b57ae9f494e10ebcc62bf2b71aa82ebadc5
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-19 14:01:47 +0430

    Change profie link address - 'Login' to 'dashboard' =>  src/layout/Header.tsx

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -66,7 +66,7 @@ const Header = () => {
                          </svg>
                          بلیط های من
                     </Link>
-                    <Link href={'/auth/login'} className="hidden lg:flex whitespace-nowrap text-gray-500 hover:bg-red-50 hover:text-red-600 duration-150 gap-x-1 items-center py-2 px-2 rounded-lg font-iranyekan-regular text-xs">
+                    <Link href={'/dashboard/edit_profile'} className="hidden lg:flex whitespace-nowrap text-gray-500 hover:bg-red-50 hover:text-red-600 duration-150 gap-x-1 items-center py-2 px-2 rounded-lg font-iranyekan-regular text-xs">
                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                          </svg>
